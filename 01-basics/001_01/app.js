f1345f1784558b1e01500de9e5cf6659ee33336e
Анастасия Моисеева <moiseeva@only.com.ru>
2025-11-12 14:17:04 +0300

    feature: base vue notes-list

--- 01-basics/001_01/app.js
+++ 01-basics/001_01/app.js
@@ -1,8 +1,36 @@
 const App = {
     data() {
         return {
-            counter: 0,
-            title: 'Счетчик'
+            title: 'Список заметок',
+            placeholderString: 'Введине название заметки',
+            inputValue: '',
+            notes: ['Заметка 1', 'Заметка 2']
+        }
+    },
+    methods: {
+        addNewNote() {
+            if(this.inputValue !== '') {
+                this.notes.push(this.inputValue);
+                this.inputValue = '';
+            }
+        },
+        toUpperCase(item) {
+            return item.toUpperCase();
+        },
+        removeNote(i) {
+            this.notes.splice(i, 1);
+        }
+    },
+    computed: {
+        doubleCountComputed() {
+            return this.notes.length * 2;
+        },
+    },
+    watch: {
+        inputValue(value) {
+            if(value.length > 10) {
+                this.inputValue = '';
+            }
         }
     }
 }
