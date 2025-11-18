f626a7b3476b335d5c27b031e5a19ff6e035b6b5
Анастасия Моисеева <moiseeva@only.com.ru>
2025-11-18 15:30:08 +0300

    feature: app-slots

---
+++ 02-create-app/src/AppBlock.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <slot name="header"></slot>
+    <slot>
+        Нет текста
+    </slot>
+    <div v-if="$slots.footer">
+        <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
